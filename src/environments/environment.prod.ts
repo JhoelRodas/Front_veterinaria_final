8441bc841b1bc909172ff7b76920e547d50674a3
Jhoel Rodas <rodasjhoel@gmail.com>
2024-12-03 04:50:29 -0400

    dfsaf

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,8 +3,15 @@
 // `ng build --env=prod` then `environment.prod.ts` will be used instead.
 // The list of which env maps to which file can be found in `angular-cli.json`.
 
+// export const environment = {
+//   production: true,
+//  backend: 'https://backend-veterinaria-final.onrender.com', // Put your backend here
+//   apiLogin: '/auth/signin',
+// };
 export const environment = {
   production: true,
- backend: 'https://backend-veterinaria-final.onrender.com', // Put your backend here
+  // backend: 'http://localhost:8081', // Put your backend here
+  //backend: 'https://ab89-158-172-227-0.ngrok-free.app', // Put your backend here
+  backend: 'https://backend-veterinaria-final.onrender.com', 
   apiLogin: '/auth/signin',
 };
